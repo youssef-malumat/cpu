911f49b793308f85c31df293204a18dfc89362a6
youssef-malumat <80450430+youssef-malumat@users.noreply.github.com>
2021-03-13 07:25:43 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qeupztpw6dqukfr8fsax4hpvfe7jmxgmq05f4fd.test1
 
 
 done
